78624f326b36261ad3da192d79aa31fd3060f795
ZaneKidwell <zanexkidwell@gmail.com>
2025-06-04 22:31:31 -0400

    moved header to it's own component

--- components/App.vue
+++ components/App.vue
@@ -411,28 +411,7 @@ const transformEventSourcesResponse = (eventSources: Ref<Record<string, any>>) =
 <template>
   <ModalsContainer />
   <div class="calendar-container">
-    <div class="logo-button-row">
-      <div class="logo-wrapper">
-        <img
-          :src="pngLogo"
-          alt="DMV DIY Logo"
-          class="logo-image"
-        />
-      </div>
-      <div class="submit-wrapper">
-        <a
-          href="https://form.jotform.com/dmvdiyfyi/dmv-diy-calendar-submission"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="/css/dmvdiysubmitbutton.png"
-            alt="Submit Event"
-            class="submit-button-image"
-          />
-        </a>
-      </div>
-    </div>
+    <LogoHeader />
     <div style="text-align: center;" v-if="beforeMOTDDate">
     </div>
     <FullCalendar ref="calendarRef" :options="calendarOptions" />
---
+++ components/LogoHeader/LogoHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="logo-button-row">
+    <div class="logo-wrapper">
+      <img
+        src="/css/dmvdiylogo.png"
+        alt="DMV DIY Logo"
+        class="logo-image"
+      />
+    </div>
+    <div class="submit-wrapper">
+      <a
+        href="https://form.jotform.com/dmvdiyfyi/dmv-diy-calendar-submission"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="/css/dmvdiysubmitbutton.png"
+          alt="Submit Event"
+          class="submit-button-image"
+        />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ logoSrc: string }>();
+</script>
+
+<style scoped src="./LogoHeader.css" />
