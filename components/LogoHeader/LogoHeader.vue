2339bcbf4fcf7ab82f9d1833f50a69238d3a5f52
ZaneKidwell <zanexkidwell@gmail.com>
2025-06-15 16:12:25 -0400

    logo header updates

--- components/LogoHeader/LogoHeader.vue
+++ components/LogoHeader/LogoHeader.vue
@@ -1,26 +1,46 @@
 <template>
-  <div class="logo-button-row">
-    <div class="logo-wrapper">
-      <img
-        src="/css/dmvdiylogo.png"
-        alt="DMV DIY Logo"
-        class="logo-image"
-      />
+  <div class="header-c">
+    <img src="/css/dmvdiylogo.png" class="logo" />
+    <div class="text-block">
+      <div class="title">DC Metro Area Community Calendar</div>
+      <div class="links">About | Resources | Submit</div>
     </div>
-    <div class="submit-wrapper">
-      <a
-        href="https://form.jotform.com/dmvdiyfyi/dmv-diy-calendar-submission"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          src="/css/dmvdiysubmitbutton.png"
-          alt="Submit Event"
-          class="submit-button-image"
-        />
-      </a>
+    <div class="buttons">
+      <img src="/css/dmvdiysubmitbutton.png" class="btn" />
     </div>
   </div>
 </template>
 
-<style scoped src="./LogoHeader.css" />
+<style scoped>
+.header-c {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+}
+
+.logo { height: 100px; }
+
+.text-block {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+
+.title {
+  font-size: 1.6rem;
+  font-weight: bold;
+}
+
+.links {
+  margin-top: 0.25rem;
+  font-size: 1rem;
+}
+
+.buttons {
+  display: flex;
+  gap: 0.5rem;
+}
+
+.btn { height: 80px; }
+</style>
