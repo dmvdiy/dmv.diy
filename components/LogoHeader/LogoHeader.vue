153fa8896f61f0d9b1d0bffce1231d4a2ad152f7
ZaneKidwell <zanexkidwell@gmail.com>
2025-06-15 16:26:08 -0400

    fixed again and better

--- components/LogoHeader/LogoHeader.vue
+++ components/LogoHeader/LogoHeader.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="header-c">
-    <img src="/css/dmvdiylogo.png" class="logo" />
-    <div class="text-block">
-      <div class="title">DC Metro Area Community Calendar</div>
-      <div class="links">About | Resources | Submit</div>
+    <div class="left-section">
+      <img src="/css/dmvdiylogo.png" class="logo" />
+      <div class="text-block">
+        <div class="title">DC Metro Area Community Calendar</div>
+        <div class="links">About | Resources | Submit</div>
+      </div>
     </div>
     <div class="buttons">
       <img src="/css/dmvdiysubmitbutton.png" class="btn" />
@@ -11,36 +13,4 @@
   </div>
 </template>
 
-<style scoped>
-.header-c {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 2rem;
-}
-
-.logo { height: 100px; }
-
-.text-block {
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-}
-
-.title {
-  font-size: 1.6rem;
-  font-weight: bold;
-}
-
-.links {
-  margin-top: 0.25rem;
-  font-size: 1rem;
-}
-
-.buttons {
-  display: flex;
-  gap: 0.5rem;
-}
-
-.btn { height: 80px; }
-</style>
+<style scoped src="./LogoHeader.css" />
