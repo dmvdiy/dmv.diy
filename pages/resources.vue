1e46a33d2e437313e1af0a66e33a0c7df8d6f4c5
DMV DIY <dmv.diy.fyi@gmail.com>
2026-05-17 11:49:26 -0400

    Update resources.vue

--- pages/resources.vue
+++ pages/resources.vue
@@ -22,23 +22,57 @@ useHead({
         <a href="mailto:dmv.diy.fyi@gmail.com">dmv.diy.fyi@gmail.com</a> if you have recommendations to add!
       </p>
 
-      <h2>Independent Bookstores, Small Press, Etc.:</h2>
+      <h2>Independent Bookstores:</h2>
       <ul>
         <li><a href="https://www.solidstatebooksdc.com/" target="_blank">Solid State Books</a></li>
         <li><a href="https://www.sankofa.com/" target="_blank">Sankofa Video, Books and Cafe</a></li>
         <li><a href="https://www.loyaltybookstores.com/meettheteam" target="_blank">Loyalty Books and Culture</a></li>
         <li><a href="https://www.mahoganybooks.com/" target="_blank">Mahogany Books</a></li>
         <li><a href="https://pottershousedc.org/bookstore" target="_blank">The Potter's House Books</a></li>
-        <li><a href="https://www.bol.coop/" target="_blank">Bol Co-Op</a> DC's first worker owned bookstore!</li>
+        <li><a href="https://www.bol.coop/" target="_blank">Bol Co-Op</a> DC's first worker-owned bookstore!</li>
         <li><a href="https://lostcitybookstore.com/" target="_blank">Lost City Books</a></li>
         <li><a href="https://peoplesbooktakoma.com/" target="_blank">The People’s Book</a></li>
         <li><a href="https://www.jfbooks.org/about" target="_blank">JF Books</a> (specializes in Asian and Asian American works)</li>
         <li><a href="https://www.middleeastbooks.com/pages/about-us" target="_blank">Middle East Books and More</a></li>
         <li><a href="https://littledistrictbooks.com/pages/store-information" target="_blank">Little District Books</a> (specializes in LGBTQ+ works)</li>
         <li><a href="https://stores.comichub.com/fantom_comics" target="_blank">Fantom Comics</a></li>
-        <li><a href="https://sunscinema.com/" target="_blank">Sun's Cinema</a> (independent movie theater and bar)</li>
-        <li><a href="https://lilacperil.com/?fbclid=PAZXh0bgNhZW0CMTEAAadfUQGElrGS6fEhGe594Lx9TS-HrsazQ8SP9GEVF9IFCx20ykUujb7O2bMOjw_aem_YOR8iA4yK_roI1m5t0CCPg" target="_blank">Lilac Press</a> A DC-based publisher and literary journal that centers local trans authors and poets</li>
+        <li><a href="https://www.eastcitybookshop.com/" target="_blank">East City Bookshop</a></li>
+        <li><a href="https://www.secondstorybooks.com/" target="_blank">Second Story Books</a></li>
+        <li><a href="https://www.mydeadauntsbooks.com/" target="_blank">My Dead Aunt's Books</a></li>
       </ul>
+
+      <h2>Cinema + Performing Arts:</h2>
+      <ul>
+        <li><a href="https://www.theavalon.org/" target="_blank">Avalon Theatre</a></li>
+        <li><a href="https://sunscinema.com/" target="_blank">Sun's Cinema</a> - independent movie theater + bar)</li>
+        <li><a href="https://silver.afi.com/" target="_blank">AFI Silver</a></li>
+        <li><a href="https://themiracletheatre.com/" target="_blank">Miracle Theatre</a></li>
+        <li><a href="https://www.galatheatre.org/" target="_blank">Gala Hispanic Theatre</a></li>
+        <li><a href="https://www.woollymammoth.net/" target="_blank">Wooly Mammoth</a></li>
+        
+      <h2>Local Small Press/Zines:</h2>
+      <ul>
+        <li><a href="https://demystification.org/" target="_blank">Demystification</a> - Distro + counterculture magazine</li>
+        <li><a href="https://lilacperil.com/?fbclid=PAZXh0bgNhZW0CMTEAAadfUQGElrGS6fEhGe594Lx9TS-HrsazQ8SP9GEVF9IFCx20ykUujb7O2bMOjw_aem_YOR8iA4yK_roI1m5t0CCPg" target="_blank">Lilac Press</a> A DC-based publisher + literary journal that centers local trans authors and poets</li>
+        <li><a href="https://www.shininglifepress.com/" target="_blank">Shining Life Press</a> - "Washington DC Fanzine"</li>
+        </ul>
+
+      <h2>Record Stores:</h2>
+        <li><a href="https://jointcustodydc.com/?srsltid=AfmBOoqdMWUdVnNMb4s6ELsj_buipFMQ0OLuleE4ZxjMyqXi5sUB6iBW" target="_blank">Joint Custody</a> - locations in Takoma Park, U Street; best place to get zines!!</li>
+        <li><a href="https://somrecordsdc.com/" target="_blank">Som Records</a> - 14th Street</li>
+        <li><a href="https://byrdlandrecords.com/" target="_blank">Byrdland Records</a> - Union Market</li>
+        <li><a href="https://homerulerecords.com/" target="_blank">HR Records</a> - Brightwood/Uptown, specializes in Jazz + R&B</li>
+        <li><a href="https://www.smashrecords.com/" target="_blank">Smash Records</a> - Adams Morgan</li>
+        <li><a href="https://crookedbeat.com/" target="_blank">Crooked Beat</a> - Del Ray; physical location temporarily closed but open online</li>
+        <li><a href="https://www.joesrecordparadise.com/" target="_blank">Joe's Record Paradise</a> - Silver Spring</li>
+        <li><a href="https://mojomala.shoplightspeed.com/" target="_blank">Mojomala</a> - Silver Spring</li>
+        <li><a href="https://artsoundlanguage.com/" target="_blank">Art, Sound, Language</a> - Chevy Chase; small but great curation</li><li><a href="https://cdcellarva.com/" target="_blank">CD Cellar</a> - Falls Church</li>
+        <li><a href="https://www.mobiusrecordshop.com/about" target="_blank">Mobius Records</a> - Fairfax</li>
+        <li><a href="https://www.sonidosmusicshop.com/about" target ="_blank">Sonidos! Music & More</a> - Beltsville</li>
+        <li><a href="https://www.redonionrecords.com/" target="_blank">Shady Lane Records (formerly Red Onion)</a> - Hyattsville (personal favorite PLUS Josh is so nice, hi Josh!)</li>
+        <li><a href="https://www.instagram.com/dinosaurpancakemusic/" target="_blank">Dinosaur Pancake</a> - Hyattsville</li>
+        <li><a href="https://www.youtube.com/watch?v=hDd2G_V1rzc" target="_blank">Obligatory RIP to Record Exchange, CDepot</a></li>
+        
 
       <h2>Spaces that host queer-centered events:</h2>
       <ul>
